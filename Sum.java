c548370fa40d959df96012a2ae04969618f1919b
TELERI01 <112411816+TELERI01@users.noreply.github.com>
2022-08-31 11:01:39 -0400

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -8,6 +8,10 @@ int number1 = 12;
 int number2 = 23;
 int sum = number1*number2;
 System.out.println(sum);
+int multiplication = number1*number2;
+System.out.println(multiplication);
+int subtraction = number1-number2;
+System.out.println(subtraction);
 
 
 }
