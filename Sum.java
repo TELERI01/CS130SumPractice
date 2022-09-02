dcb90b477dfb8f2614ed5d8580c6c0d515188a6c
TELERI01 <112411816+TELERI01@users.noreply.github.com>
2022-09-02 10:35:20 -0400

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -6,7 +6,7 @@ public static void main(String[] args)
 
 int number1 = 12;
 int number2 = 23;
-int sum = number1*number2;
+int sum = number1+number2;
 System.out.println(sum);
 int multiplication = number1*number2;
 System.out.println(multiplication);
